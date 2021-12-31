45547d479811c02d3e0a255bef81e26d78808d46
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2021-12-31 16:45:14 +0700

    adding Seo

---
+++ src/components/Seo.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { graphql, useStaticQuery } from "gatsby"
+
+const getTitle = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
+
+const Seo = ({ title, description }) => {
+  const findTitle = useStaticQuery(getTitle)
+  const { siteTitle } = findTitle.site.siteMetadata
+  return (
+    <Helmet
+      title={`${siteTitle} | ${title}`}
+      meta={[{ name: `description`, content: description }]}
+    ></Helmet>
+  )
+}
+
+export default Seo
