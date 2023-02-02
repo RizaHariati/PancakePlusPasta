be4d715fb18c5fd522d5de83f03f9c82a99f8405
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2023-02-02 13:44:41 +0700

    fixing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,50 +19,50 @@ const Footer = () => {
     <AppBar position="sticky" sx={footerAppbar}>
       <Toolbar sx={footerToolbar}>
         <a href="https://ichacodes.com/">
-          <Typography variant="body1" color="white">
-            <span style={{ color: theme.palette.primary.light }}>
-              {" "}
-              Pancake Plus Pasta Copyright
-            </span>{" "}
-            Riza Hariati &copy;{new Date().getFullYear()}
-          </Typography>{" "}
+          <Typography variant="body2" color="white">
+            Pancake Plus Pasta by Riza Hariati for
+            <span style={{ color: "#DD9056", textDecoration: "underline" }}>
+              &nbsp;IchaCodes&nbsp;
+            </span>
+            &nbsp;&copy;{new Date().getFullYear()}
+          </Typography>
         </a>
         <div className="footerAttribute">
           <a href="https://www.pexels.com/photo/gold-and-silver-baubles-on-white-wall-5725975/">
             <Typography variant="h6" color="whitesmoke">
-              Photo by Karolina Grabowska from Pexels
+              | Photo by Karolina Grabowska from Pexels |
             </Typography>
           </a>
           <a href="https://www.vecteezy.com/free-vector/mail">
             <Typography variant="h6" color="whitesmoke">
-              Mail Vectors by Vecteezy
+              | Mail Vectors by Vecteezy |
             </Typography>
           </a>
           <a href="https://www.canva.com/">
             <Typography variant="h6" color="whitesmoke">
-              Sliders images by Canva
+              | Sliders images by Canva |
             </Typography>
           </a>
           <a href="https://pixabay.com/">
-            <Typography variant="h6" color="whitesmoke">
-              Images by Pixabay
+            <Typography variant="h6" color="primary">
+              | Images by Pixabay |
             </Typography>
           </a>
           <a href="https://www.freepik.com/vectors/money">
-            <Typography variant="h6" color="whitesmoke">
-              Money vector created by pch.vector - www.freepik.com
+            <Typography variant="h6" color="primary">
+              | Money vector created by pch.vector - www.freepik.com |
             </Typography>
           </a>
           <a href="https://www.vecteezy.com/free-vector/web">
-            <Typography variant="h6" color="whitesmoke">
-              Web Vectors by Vecteezy
+            <Typography variant="h6" color="primary">
+              | Web Vectors by Vecteezy |
             </Typography>
           </a>
 
           <a href="https://www.vecteezy.com/free-vector/glass">
             {" "}
-            <Typography variant="h6" color="whitesmoke">
-              Glass Vectors by Vecteezy{" "}
+            <Typography variant="h6" color="primary">
+              | Glass Vectors by Vecteezy |
             </Typography>
           </a>
         </div>
